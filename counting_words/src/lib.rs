02c11ec9424972454d213314ca72b568967c2fc7
Mustapha Lbahja <mlbahja>
2025-06-28 17:29:50 +0100

    f

--- counting_words/src/lib.rs
+++ counting_words/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn counting_words(words: &str) -> HashMap<String, u32> {
+    let mut s = String::new();
+    for c in words.chars() {
+        if c.is_alphanumeric() || c.is_whitespace() || c == '\'' {
+            s.push(c)
+        }
     }
+    let s = s.replace("' ", " ").replace(" '", " ");
+    let sp = s.split_whitespace();
+    let mut mymap = HashMap::new();
+    for word in sp {
+        *mymap.entry(word.to_lowercase()).or_insert(0) += 1
+    }
+    mymap
 }
